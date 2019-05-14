4ceb9829f89442d23ba5241063b119a82343ede2
David Byers <byersdz@gmail.com>
2019-05-13 22:12:35 -0400

    adding exports

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import * as keysImport from './Input/Keys';
 
 export { default } from './Engine/Engine';
-export { default as Engine } from './Engine/Engine';
 
-export { default as Screen } from './Screen/Screen';
+export { default as Audio } from './Audio/Audio';
+export { default as Frequencies } from './Audio/Frequencies';
+export { default as Notes } from './Audio/Notes';
+export { default as Sound } from './Audio/Sound';
+
+export { default as Font } from './FontData/Font';
+export { default as FontData } from './FontData/FontData';
+
 export { default as Input } from './Input/Input';
-export { default as TileData } from './TileData/TileData';
+export const Keys = keysImport;
+
 export { default as MapData } from './MapData/MapData';
 export { default as TileMap } from './MapData/TileMap';
 
-export const Keys = keysImport;
+export { default as Engine } from './Engine/Engine';
+
+export { default as Screen } from './Screen/Screen';
+
+export { default as TileData } from './TileData/TileData';
