b4a6b777e8cc8722377ca2a129dd6a7da62ed895
David Byers <byersdz@gmail.com>
2019-12-24 13:36:52 -0500

    added array compression tests

--- src/ConvertData/ConvertData.test.js
+++ src/ConvertData/ConvertData.test.js
@@ -2,10 +2,6 @@
 import ConvertData from './ConvertData';
 
 test( 'array to run min', () => {
-  const testArray = new Array( 1000 );
-  testArray.fill( 97 );
-  const compressed = ConvertData.arrayToCompressedString( testArray );
-  ConvertData.compressedStringToArray( compressed );
   expect( ConvertData.arrayToRun( [1, 2, 3] ) ).toEqual( [1, 1, 1, 2, 1, 3] );
 } );
 
@@ -39,4 +35,25 @@ test( 'run to array mono', () => {
   const target = new Array( size );
   target.fill( 99 );
   expect( ConvertData.runToArray( source ) ).toEqual( target );
+} );
+
+test( 'array to compressed min', () => {
+  const testArray = [4, 5, 6];
+  const compressed = ConvertData.arrayToCompressedString( testArray );
+  const decompressed = ConvertData.compressedStringToArray( compressed );
+
+  expect( decompressed ).toEqual( testArray );
+} );
+
+test( 'array to match known compressed string', () => {
+  const testArray = [255, 0, 1, 5, 6, 7, 99, 128, 22, 37];
+  const compressed = ConvertData.arrayToCompressedString( testArray );
+  expect( compressed ).toEqual( 'EwVhBoAZwRnCBs4Ds4CcbbABzmMcAZmSA' );
+} );
+
+test( 'compressed string to match known array', () => {
+  const compressed = 'EwVhBoAZwRnCBs4Ds4CcbbABzmMcAZmSA';
+  const decompressed = ConvertData.compressedStringToArray( compressed );
+
+  expect( decompressed ).toEqual( [255, 0, 1, 5, 6, 7, 99, 128, 22, 37] );
 } );
